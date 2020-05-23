97316c8f10a238d10bb2f53c55d2ae603ad07528
stephensenjaya <senjaya.stephen@gmail.com>
2020-05-23 17:18:25 +0700

    [ADD] contact us section

---
+++ resources/views/section/contact_us.blade.php
@@ -0,0 +1,91 @@
+<section id="contact_us" class="team">
+	<div class="container" data-aos="fade-up">
+
+		<div class="section-title">
+			<h2>Contact US</h2>
+			<p>Check our Team</p>
+		</div>
+		<div class="row">
+
+			<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+				<div class="member" data-aos="fade-up" data-aos-delay="100">
+					<div class="member-img">
+						<img src="/assets/img/team/team-1.jpg" class="img-fluid" alt="">
+						<div class="social">
+							<a href=""><i class="icofont-twitter"></i></a>
+							<a href=""><i class="icofont-facebook"></i></a>
+							<a href=""><i class="icofont-instagram"></i></a>
+							<a href=""><i class="icofont-linkedin"></i></a>
+						</div>
+					</div>
+					<div class="member-info">
+						<h4>Richard Gunawan</h4>
+						<a href="mailto:richard.hmj@gmail.com"><span>richard.hmj@gmail.com</span></a>					
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+				<div class="member" data-aos="fade-up" data-aos-delay="200">
+					<div class="member-img">
+						<img src="/assets/img/team/team-2.jpg" class="img-fluid" alt="">
+						<div class="social">
+							<a href=""><i class="icofont-twitter"></i></a>
+							<a href=""><i class="icofont-facebook"></i></a>
+							<a href=""><i class="icofont-instagram"></i></a>
+							<a href=""><i class="icofont-linkedin"></i></a>
+						</div>
+					</div>
+					<div class="member-info">
+						<h4>Agus Prihanto</h4>
+						<a href="mailto:agus.prihanto@hanamaster.co.id"><span>agus.prihanto@hanamaster.co.id</span></a>
+
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+				<div class="member" data-aos="fade-up" data-aos-delay="300">
+					<div class="member-img">
+						<img src="/assets/img/team/team-3.jpg" class="img-fluid" alt="">
+						<div class="social">
+							<a href=""><i class="icofont-twitter"></i></a>
+							<a href=""><i class="icofont-facebook"></i></a>
+							<a href=""><i class="icofont-instagram"></i></a>
+							<a href=""><i class="icofont-linkedin"></i></a>
+						</div>
+					</div>
+					<div class="member-info">
+						<h4>Gholib Mustofa</h4>
+						<a href="mailto:gholibmustofa@yahoo.com"><span>gholibmustofa@yahoo.com</span></a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+				<div class="member" data-aos="fade-up" data-aos-delay="400">
+					<div class="member-img">
+						<img src="/assets/img/team/team-4.jpg" class="img-fluid" alt="">
+						<div class="social">
+							<a href=""><i class="icofont-twitter"></i></a>
+							<a href=""><i class="icofont-facebook"></i></a>
+							<a href=""><i class="icofont-instagram"></i></a>
+							<a href=""><i class="icofont-linkedin"></i></a>
+						</div>
+					</div>
+					<div class="member-info">
+						<h4>Steven Gunawan</h4>
+						<a href="mailto:steven.hg@hanamaster.co.id"><span>steven.hg@hanamaster.co.id</span></a>
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<hr>
+		<div>
+			<iframe style="border:0; width: 100%; height: 270px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.0760294489723!2d107.49498561407954!3d-6.881495395027322!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e4dbaab21481%3A0x3e6715d778acd91f!2sPT%20Hana%20Master%20Jaya!5e0!3m2!1sen!2sid!4v1590090585296!5m2!1sen!2sid" frameborder="0" allowfullscreen></iframe>
+		</div>
+	</div>
+
+
+</section>
