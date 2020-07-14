1fa14d8b75548eb7284e809b015d0de299ed6a72
hengky <hengkihan@yahoo.com>
2020-07-14 22:48:01 +0700

    [REMOVE] Socmed Footer and Change Header Color

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -166,11 +166,7 @@
 								<strong>Email:</strong> info@hanamaster.co.id<br>
 							</p>
 							<div class="social-links mt-3">
-								<a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-								<a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-								<a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-								<a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-								<a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+								<a target="_blank" href="https://www.linkedin.com/company/pt-hana-master-jaya" class="linkedin"><i class="bx bxl-linkedin"></i></a>
 							</div>
 						</div>
 					</div>
